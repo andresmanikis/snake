086d8cbbdd625138620c5e8c4070c87b5096c68c
Andrés Manikis <andresmanikis@gmail.com>
2021-05-09 21:16:52 +0100

    Extracts functions

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,42 @@ import { Board } from "./Board";
 import { handleKey } from "./handleKey";
 import { BumpedIntoMyselfError, Direction, Snake } from "./Snake";
 
-function App() {
-  const snakeRef = useRef(
-    new Snake([
-      [0, 0],
-      [0, 1],
-      [0, 2],
-      [1, 2],
-      [2, 2],
-      [2, 3],
-      [2, 4],
-      [2, 5],
-      [2, 6],
-      [3, 6],
-    ])
-  );
+function createSnake(): Snake {
+  return new Snake([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [1, 2],
+    [2, 2],
+    [2, 3],
+    [2, 4],
+    [2, 5],
+    [2, 6],
+    [3, 6],
+  ]);
+}
 
+function App() {
+  const snakeRef = useRef(createSnake());
   const [stateMatrix, setStateMatrix] = useState<boolean[][]>(
     updateStateMatrix()
   );
-
   const [gameOver, setGameOver] = useState<boolean>(false);
 
   function onKeydown(e: KeyboardEvent) {
     handleKey(e, snakeRef.current);
   }
 
-  useEffect(() => {
+  function registerKeyboardListener() {
     document.addEventListener("keydown", onKeydown);
-
     return () => document.removeEventListener("keydown", onKeydown);
-  }, []);
+  }
 
-  useEffect(() => {
+  function setStartingDirection() {
     snakeRef.current.setDirection(Direction.Right);
-  }, []);
+  }
 
-  useEffect(() => {
+  function registerInterval() {
     const interval = setInterval(() => {
       try {
         snakeRef.current.move();
@@ -55,11 +54,15 @@ function App() {
     }, 100);
 
     return () => clearInterval(interval);
-  }, []);
+  }
 
   function updateStateMatrix() {
     return asMatrix(snakeRef.current, 10, 20);
   }
+
+  useEffect(registerKeyboardListener, []);
+  useEffect(setStartingDirection, []);
+  useEffect(registerInterval, []);
 
   if (gameOver) {
     return <div>GAME OVER :(</div>;
