c86b542df73f1a6367e809c1043bc67b306def33
Andrés Manikis <andresmanikis@gmail.com>
2021-05-09 18:03:30 +0100

    Shows example in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
+import { useEffect, useRef, useState } from "react";
+
 import { asMatrix } from "./asMatrix";
 import { Board } from "./Board";
-import { Snake } from "./Snake";
+import { Direction, Snake } from "./Snake";
 
 function App() {
-  const snake = new Snake([
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [1, 2],
-    [2, 2],
-    [2, 3],
-    [2, 4],
-    [2, 5],
-    [2, 6],
-    [3, 6],
-  ]);
+  const snakeRef = useRef(
+    new Snake([
+      [0, 0],
+      [0, 1],
+      [0, 2],
+      [1, 2],
+      [2, 2],
+      [2, 3],
+      [2, 4],
+      [2, 5],
+      [2, 6],
+      [3, 6],
+    ])
+  );
 
-  return <Board stateMatrix={asMatrix(snake, 10, 10)} />;
+  const [stateMatrix, setStateMatrix] = useState<boolean[][]>(
+    updateStateMatrix()
+  );
+
+  useEffect(() => {
+    snakeRef.current.setDirection(Direction.Right);
+    setInterval(() => {
+      snakeRef.current.move();
+      setStateMatrix(updateStateMatrix);
+    }, 100);
+  }, []);
+
+  function updateStateMatrix() {
+    return asMatrix(snakeRef.current, 10, 20);
+  }
+
+  return <Board stateMatrix={stateMatrix} />;
 }
 
 export default App;
